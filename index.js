bb999b24576bbf06b4a0eb45ed7cee52a2d1a74c
Saleh Yassin <yass0016@gmail.com>
2017-07-31 12:35:39 +0000

    Added abs path to the static folder in index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var config = require('./config');
-
 var helloworld = require('./routes/helloworld');
 
 const app = express();
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   res.render('index.pug', { title: 'Home' });
 });
 
-app.use('/helloworld', helloworld); 
-app.use(express.static('static'));
+app.use('/helloworld', helloworld);
+app.use(express.static(__dirname + '/static'));
 
 app.listen(config.port, (err) => {
   if(err)
